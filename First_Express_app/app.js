4a101aa942407c01b1f95925234ae5b31699b6fd
NKwatra <kwatranishkarsh@gmail.com>
2019-10-02 14:08:28 +0530

    Create basic app

--- First_Express_app/app.js
+++ First_Express_app/app.js
@@ -1,1 +1,18 @@
-console.log("This is the first express app");
+var express = require('express');
+var app = express();
+
+app.get("/", function(req,res){
+    res.send("Hi there!!!");
+});
+
+app.get("/bye", function(req,res){
+    res.send("Goodbye !!!");
+})
+
+app.get("/cat", function(req,res){
+    res.send("Meow");
+})
+
+app.listen(3000, function(){
+    console.log("Server has started !!!");
+});
